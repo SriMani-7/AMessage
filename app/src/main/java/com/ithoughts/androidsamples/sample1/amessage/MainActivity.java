e313d1a686e3060a6b9b5ea01be5a4ee7fc90c8b
SriMani-7 <basinenisrimanikanta@gmail.com>
2021-09-10 18:43:55 +0530

    Basic project setup
    Creating a new android studio project and some configurations to it.

---
+++ app/src/main/java/com/ithoughts/androidsamples/sample1/amessage/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ithoughts.androidsamples.sample1.amessage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
